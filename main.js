98d57f8d2c993b8c387ff4feb8aa4feabace2b66
Calamital <128102136+Calamital@users.noreply.github.com>
2026-09-20 01:53:02 +0000

    a

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("input").addEventListener("keyup", function(event) {
+    if (event.key = "Enter") {
+        if (event.target.value == "5107") {
+            document.getElementById("next").innerHTML = "alright youve made it through. here you go! https://docs.google.com/presentation/d/1I0Ei4SDMWyKTT5xZMU4QnqxQlTcPhdVCmQ3qVGpWg84/"
+        }
+    }
+})
